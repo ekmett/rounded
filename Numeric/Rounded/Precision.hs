36d036d696051e3b00e94c9e73c7cdda62b29d2a
Edward Kmett <ekmett@gmail.com>
2012-09-03 17:03:08 -0400

    bits ensures we actually have >= 2 bits

--- Numeric/Rounded/Precision.hs
+++ Numeric/Rounded/Precision.hs
@@ -1,8 +1,5 @@
-{-# LANGUAGE CPP, EmptyDataDecls, FlexibleContexts, MultiParamTypeClasses, UndecidableInstances, TemplateHaskell, Rank2Types, MagicHash #-}
+{-# LANGUAGE EmptyDataDecls, FlexibleContexts, MultiParamTypeClasses, UndecidableInstances, TemplateHaskell, Rank2Types, MagicHash #-}
 
-#ifndef MIN_VERSION_template_haskell
-#define MIN_VERSION_template_haskell(x,y,z) (defined(__GLASGOW_HASKELL__) && __GLASGOW_HASKELL__ > 706)
-#endif
 module Numeric.Rounded.Precision
     ( Precision(..)
     , reifyPrecision
@@ -20,7 +17,7 @@ import Data.Proxy
 
 class Precision p where
     precision :: proxy p -> Int
-    prec# :: proxy p -> Int# 
+    prec# :: proxy p -> Int#
     prec# p = case precision p of
       I# i# -> i#
 
@@ -44,6 +41,7 @@ data PrecSucc a
 
 retagSucc :: (Proxy n -> a) -> proxy (PrecSucc n) -> a
 retagSucc f _ = f Proxy
+{-# INLINE retagSucc #-}
 
 instance Precision n => Precision (PrecSucc n) where
     precision = (1+) <$> retagSucc precision 
@@ -52,32 +50,26 @@ data PrecDouble a
 
 retagDouble :: (Proxy n -> a) -> proxy (PrecDouble n) -> a
 retagDouble f _ = f Proxy
+{-# INLINE retagDouble #-}
 
 instance Precision n => Precision (PrecDouble n) where
-    precision = (2*) <$> retagDouble precision 
+    precision = (2*) <$> retagDouble precision
 
 -- |
 -- A Precision for a specified number of bits.
 --
 -- > type Huge r = Rounded r $(bits 512)
 bits :: Int -> Q Type
-bits 0 = conT ''PrecZero
-bits n = case divMod n 2 of
-        (q,0) -> conT ''PrecDouble `appT` bits q
+bits m = go (max m 2) where
+  go 0 = conT ''PrecZero
+  go n = case divMod n 2 of
+        (q,0) -> conT ''PrecDouble `appT` go q
         (0,1) -> conT ''PrecSucc `appT` conT ''PrecZero
-        (q,1) -> conT ''PrecSucc `appT` (conT ''PrecDouble `appT` bits q)
-        (_,_) -> err "bits: A negative number of bits were requested"
+        (q,1) -> conT ''PrecSucc `appT` (conT ''PrecDouble `appT` go q)
+        (_,_) -> error "ghc is bad at math"
 
 bytes :: Int -> Q Type
 bytes = bits . (*8)
-
-err :: String -> Q a
-#if MIN_VERSION_template_haskell(2,8,0)
-err = reportError
-#else
-err = report True
-#endif
-
 
 data ReifiedPrecision s
 
