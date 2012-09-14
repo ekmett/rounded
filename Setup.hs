706706aead6c4d2e942106e7cf8ffc25b3f84742
Edward Kmett <ekmett@gmail.com>
2012-09-14 18:52:03 -0400

    initial stab at autoconf

--- Setup.hs
+++ Setup.hs
@@ -27,18 +27,6 @@ import Data.Maybe
 -- Horrible mess of semi-general code
 --
 
--- Not particularly thread safe, but the whole notion of a current directory isn't either
-inDirectory :: FilePath -> IO r -> IO r
-inDirectory dir action = do
-  old <- getCurrentDirectory
-  setCurrentDirectory dir
-  res <- action
-  setCurrentDirectory old
-  return res
-
-mpfrVersion = "3.1.0"
-mpfrRoot = "deps/mpfr-" ++ mpfrVersion
-
 -- TODO: support Windows nicely
 runOrBomb :: FilePath -> [String] -> IO ()
 runOrBomb cmd args = do
@@ -51,94 +39,42 @@ runOrBomb cmd args = do
       hPutStrLn stderr $ err
       exitWith $ ExitFailure e
 
-getDist = do
-  -- let Flag relDistDir = Flag defaultDistPref `mappend` configDistPref flags
-  -- canonicalizePath relDistDir
-  canonicalizePath "dist" -- ugly. There must be a safer way to get the absolute path of the dist dir
+getDist = canonicalizePath "dist" -- ugly. There must be a safer way to get the absolute path of the dist dir
 
 createDirectory' dir = do
-  exists <- doesDirectoryExist $ dir
+  exists <- doesDirectoryExist dir
   unless exists $ createDirectory dir -- let's hope nobody creates the directory first!
-
-configureMpfr :: FilePath -> IO ()
-configureMpfr distDir =
-  inDirectory mpfrRoot $ do
-    putStrLn $ "--> Configuring MPFR " ++ mpfrVersion ++ "..."
-    runOrBomb "sh" ["configure", "--prefix=" ++ distDir]
-
-makeMpfr :: FilePath -> IO ()
-makeMpfr distDir =
-  inDirectory mpfrRoot $ do
-    putStrLn $ "--> Building MPFR " ++ mpfrVersion ++ "..."
-    runOrBomb "make" []
-    runOrBomb "make" ["install"]
 
 mpfrHooks :: UserHooks
 mpfrHooks = autoconfUserHooks
     { preConf   = mpfrPreConf
     , postConf  = mpfrPostConf
-    , confHook  = mpfrConfHook
-    , preBuild  = mpfrPreBuild
-    , postBuild = mpfrPostBuild
     , postClean = mpfrPostClean
     }
   where
-  mpfrConfHook (pkg, pbi) flags = do
-    distDir <- getDist
-    lbi <- confHook autoconfUserHooks (pkg, pbi) flags
-    let lpd = localPkgDescr lbi
-        lib = fromJust (library lpd)
-        libbi = libBuildInfo lib
-        libbi' = libbi { extraLibDirs = (distDir </> "lib") : extraLibDirs libbi }
-        lib' = lib { libBuildInfo = libbi' }
-        lpd' = lpd { library = Just lib' }
-    return lbi { localPkgDescr = lpd' }
- 
   -- We need to create the "include" directory at some point, but we're doing it this early to make cabal
   -- shut up about it not being present.
   mpfrPreConf args flags = do
     distDir <- getDist
     createDirectory' $ distDir </> "include"
-    createDirectory' $ distDir </> "lib"
-    createDirectory' $ distDir </> "tmp"
-    return emptyHookedBuildInfo
+    preConf autoconfUserHooks args flags
 
-  mpfrPostConf args flags pkg_descr lbi = do
-    postConf simpleUserHooks args flags pkg_descr lbi
-    configureMpfr =<< getDist
-
-  mpfrPreBuild args flags = do
-    preBuild simpleUserHooks args flags
+  -- Should we move this logic into configure.ac?
+  mpfrPostConf args flags pkgd lbi = do
     distDir <- getDist
-    makeMpfr distDir
     putStrLn $ "Determining MPFR constants..."
     programExists <- doesFileExist $ distDir </> "mkMpfrDerivedConstants"
     unless programExists $ do
       Just gcc <- programFindLocation gccProgram normal
-      runOrBomb gcc ["cbits/mkMpfrDerivedConstants.c", "-I" ++ distDir </> "include", "-o", distDir </> "mkMpfrDerivedConstants"]
+      runOrBomb gcc ["cbits/mkMpfrDerivedConstants.c", "-Impfr/src", "-I" ++ distDir </> "include", "-o", distDir </> "mkMpfrDerivedConstants"]
     headerExists <- doesFileExist $ distDir </> "include" </> "MpfrDerivedConstants.h"
     unless headerExists $ do
       header <- readProcess (distDir </> "mkMpfrDerivedConstants") [] ""
       writeFile (distDir </> "include" </> "MpfrDerivedConstants.h") header
-    return emptyHookedBuildInfo
-
-  mpfrPostBuild args flags pkg_descr lbi = do
-    distDir <- getDist
-    (ar, _) <- requireProgram silent arProgram defaultProgramDb
-
-    putStrLn "Mangling static library..."
-    inDirectory (distDir </> "tmp") $ do
-      runOrBomb "ar" ["-x", distDir </> "build" </> "libHSrounded-0.0.1.a"]
-      runOrBomb "ar" ["-x", distDir </> "lib" </> "libmpfr.a"]
-
-    objects <- map ((distDir </> "tmp") </>) <$> filter (".o" `isSuffixOf`) <$> getDirectoryContents (distDir </> "tmp")
-
-    createArLibArchive silent ar (distDir </> "build" </> "libHSrounded-0.0.1.a") objects
-
-    postBuild simpleUserHooks args flags pkg_descr lbi
+    postConf autoconfUserHooks args flags pkgd lbi
 
   mpfrPostClean args flags pkg_descr _ = do
-    inDirectory mpfrRoot (readProcessWithExitCode "make" ["distclean"] "")
+    readProcessWithExitCode "make" ["distclean"] ""
     return ()
 
 main :: IO ()
